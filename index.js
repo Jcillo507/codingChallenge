8b66a8b719c94adbf619bfa2eee27c3fb816a6fa
Joe Cillo <JCillo507@yahoo.com>
2020-06-30 21:15:17 -0400

    mvp

--- index.js
+++ index.js
@@ -1,87 +1,88 @@
-let imageData = [];
-let currentPage = 1;
-let imagesPerPage = 10;
+'use stict'
 
-//there are 2 calls being made because each call returns 30 photos
-let call1 = fetch(`https://api.unsplash.com/search/photos?client_id=4SWIpzz-ZrDk8XREmTZZAmYYb-819DU3txSfFnVBxMQ&query=puppies&per_page=50/&page=1`
-).then(res => res.json())
+const imageData = []
+let currentPage = 1
+const imagesPerPage = 10
+
+// there are 2 calls being made because each call returns 30 photos
+const call1 = fetch('https://api.unsplash.com/search/photos?client_id=4SWIpzz-ZrDk8XREmTZZAmYYb-819DU3txSfFnVBxMQ&query=puppies&per_page=50/&page=1').then((res) => res.json())
   .then((data) => {
     imageData.push(...data.results)
   })
 
-let call2 = fetch(`https://api.unsplash.com/search/photos?client_id=4SWIpzz-ZrDk8XREmTZZAmYYb-819DU3txSfFnVBxMQ&query=puppies&per_page=50/&page=2`
-).then(res => res.json())
+const call2 = fetch('https://api.unsplash.com/search/photos?client_id=4SWIpzz-ZrDk8XREmTZZAmYYb-819DU3txSfFnVBxMQ&query=puppies&per_page=50/&page=2').then((res) => res.json())
   .then((data) => {
     imageData.push(...data.results)
   })
 
 const createPage = (page) => {
-  console.log(imageData)
-  const nextBtn = document.getElementById('next-btn');
-  const prevBtn = document.getElementById('prev-btn');
-  const root = document.getElementById('root');
-  const pageNumber = document.getElementById('page-number');
+  const nextBtn = document.getElementById('next-btn')
+  const prevBtn = document.getElementById('prev-btn')
+  const root = document.getElementById('root')
+  const pageNumber = document.getElementById('page-number')
   const modal = document.getElementById('modal')
   const modalImg = document.getElementById('modal-img')
 
-  if (page < 1) page = 1;
-  if (page > numPages()) page = numPages();
+  if (page < 1) page = 1
+  if (page > numPages()) page = numPages()
 
-  root.innerHTML = '';
+  root.innerHTML = ''
 
   for (let i = (page - 1) * imagesPerPage; i < (page * imagesPerPage) && i < imageData.length; i++) {
     const createImg = document.createElement('img')
+
     createImg.src = imageData[i].urls.full
     createImg.setAttribute('class', 'image-thumbnail')
     createImg.setAttribute('id', imageData[i].id)
     createImg.setAttribute('alt', imageData[i].alt_description)
     root.appendChild(createImg)
     const getImage = document.getElementById(imageData[i].id)
+
     getImage.addEventListener('click', () => {
       modal.style.display = 'block'
       modalImg.src = imageData[i].urls.full
-      if(imageData[i].height>3000){
-      modalImg.setAttribute('class', 'image-modal-tall')}
-      else{
-      modalImg.setAttribute('class', 'image-modal')}
+      if (imageData[i].height > 3000) {
+        modalImg.setAttribute('class', 'image-modal-tall')
+      } else {
+        modalImg.setAttribute('class', 'image-modal')
+      }
     })
     const closeBtn = document.getElementById('close-btn')
+
     closeBtn.addEventListener('click', () => {
       modal.style.display = 'none'
     })
   }
 
-  pageNumber.innerHTML = page + '/' + numPages();
+  pageNumber.innerHTML = `${page} of ${numPages()} pages`
 
-  if (page == 1) {
-    prevBtn.style.visibility = 'hidden';
+  if (page === 1) {
+    prevBtn.style.visibility = 'hidden'
   } else {
-    prevBtn.style.visibility = 'visible';
+    prevBtn.style.visibility = 'visible'
   }
 
-  if (page == numPages()) {
-    nextBtn.style.visibility = 'hidden';
+  if (page === numPages()) {
+    nextBtn.style.visibility = 'hidden'
   } else {
-    nextBtn.style.visibility = 'visible';
+    nextBtn.style.visibility = 'visible'
   }
 }
 
-const numPages = () => {
-  return Math.ceil(imageData.length / imagesPerPage);
-}
+const numPages = () => Math.ceil(imageData.length / imagesPerPage)
 
 const prevPage = () => {
   if (currentPage > 1) {
-    currentPage--;
-    createPage(currentPage);
+    currentPage--
+    createPage(currentPage)
   }
 }
 
 const nextPage = () => {
   if (currentPage < numPages()) {
-    currentPage++;
-    createPage(currentPage);
+    currentPage++
+    createPage(currentPage)
   }
 }
 
-Promise.all([call1, call2]).then(() => createPage(1));
+Promise.all([call1, call2]).then(() => createPage(1))
--- server.js
+++ server.js
@@ -1,16 +1,13 @@
+'use stict'
 
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const port = 3000;
-const app = express();
+const port = 3000
+const app = express()
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
-app.use(express.static(__dirname), () => {
-  console.log('Request Recived');
-});
-app.listen(port, () => {
-  console.log('Listening on Port ', port);
-});
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
+app.use(express.static(__dirname))
+app.listen(port)
